c69a2632d0d2f0658b137eb6365520f44db56f6a
Cierra <ceceminorhampton@gmail.com>
2023-05-16 17:48:37 -0400

    added the setsum arthur

--- src/main/java/org/example/IOU.java
+++ src/main/java/org/example/IOU.java
@@ -18,7 +18,7 @@ public class IOU {
         IOU mattsIOU = new IOU();
         mattsIOU.setSum("Arthur", 51.5);
         mattsIOU.setSum("Michael", 30);
-//        mattsIOU.setSum("Arthur", 10.5);
+        mattsIOU.setSum("Arthur", 10.5);
 
         System.out.println(mattsIOU.howMuchDoIOweTo("Arthur"));
         System.out.println(mattsIOU.howMuchDoIOweTo("Michael"));
